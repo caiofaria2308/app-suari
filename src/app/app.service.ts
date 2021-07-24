3768e3883b097441732de6e3b774e5e23429ada9
Caio de Faria <caiofaria2308@gmail.com>
2021-07-24 13:41:44 -0300

    fix link api

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -19,7 +19,7 @@ export class AppService {
   }
   login(form: any){
     
-    var url = "http://127.0.0.1:8000/auth/"
+    var url = `${localStorage.getItem('url_api')}auth/`
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type':  'application/json'
